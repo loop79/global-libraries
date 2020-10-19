fb0a248dbc8ee5f19fa0c7531be4e072fc2e76de
loop79 <65086088+loop79@users.noreply.github.com>
2020-10-19 18:36:57 +0200

    test

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -7,7 +7,12 @@ def call(Map params) {
 
 //log.info(message: "Uploading to openshift")
   sh '''  
-ls -l  
+GIT_COMMIT=$(git rev-parse HEAD) 
+echo -e "
+LABEL io.openshift.build.commit.vendor=\'loop software\'
+LABEL io.openshift.build.commit.url=\'${GIT_URL}\'
+LABEL io.openshift.build.commit.id=\'${GIT_COMMIT}\'
+LABEL io.openshift.build.commit.ref=\'${BRANCH_NAME}\'" >> Dockerfile
 oc new-build --strategy docker --binary --name myapp2 --to="myapp2"
 oc start-build myapp2 --from-dir . --follow
 oc new-app myapp2
